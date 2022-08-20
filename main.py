59c997912f1a588fc8ed7bd8831a20396c545d87
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2022-08-20 08:28:42 -0700

    tuning widths

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import utils
 HOST = 'https://tanjeffreyz-github-overview.herokuapp.com'
 GITHUB_STATISTICS = 'https://github.com/tanjeffreyz/github-statistics'
 DELIMITERS = '|'.join([','])
-BANNER_WIDTH = 98
+BANNER_WIDTH = 97
 SETTINGS = {
     'order': [
         '',                 # Default value of setting
